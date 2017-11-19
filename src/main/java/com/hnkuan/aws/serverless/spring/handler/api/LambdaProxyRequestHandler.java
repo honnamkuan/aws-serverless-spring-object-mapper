293fd3ea8f7add6e48cba9fcf34ebd27370194e8
Hon Nam Kuan <honnamkuan@gmail.com>
2017-11-19 16:51:55 +0800

    Exclude commons-logging

--- src/main/java/com/hnkuan/aws/serverless/spring/handler/api/LambdaProxyRequestHandler.java
+++ src/main/java/com/hnkuan/aws/serverless/spring/handler/api/LambdaProxyRequestHandler.java
@@ -2,7 +2,6 @@ package com.hnkuan.aws.serverless.spring.handler.api;
 
 import com.amazonaws.serverless.proxy.internal.model.AwsProxyRequest;
 import com.amazonaws.serverless.proxy.internal.model.AwsProxyResponse;
-import com.hnkuan.aws.serverless.spring.adapter.LambdaRequestAdapterImpl;
 import com.hnkuan.aws.serverless.spring.adapter.api.LambdaRequestAdapter;
 import org.springframework.context.ApplicationContext;
 import org.springframework.http.ResponseEntity;
@@ -23,14 +22,22 @@ public interface LambdaProxyRequestHandler {
     ApplicationContext getContext();
 
     /**
+     * Get an instance of {@link LambdaRequestAdapter}
+     *
+     * @return The implementation of {@link LambdaRequestAdapter}
+     */
+    default LambdaRequestAdapter getLambdaRequestAdapter() {
+        return getContext().getBean(LambdaRequestAdapter.class);
+    }
+
+    /**
      * Forward {@link AwsProxyRequest} to POJO request handler.
      *
      * @param pAwsProxyRequest The {@link AwsProxyRequest}
      * @return {@link AwsProxyResponse} mapped from {@link ResponseEntity}
      */
     default AwsProxyResponse handle(AwsProxyRequest pAwsProxyRequest) {
-        return getContext().getBean(LambdaRequestAdapterImpl.class)
-                .handle(pAwsProxyRequest);
+        return getLambdaRequestAdapter().handle(pAwsProxyRequest);
     }
 
 }
